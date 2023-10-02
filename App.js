d2aaae1c98f2e9e9be83be4b6fa24dff33f40d16
OrleansParishIT <joseph.adogeri@orleansassessors.com>
2023-10-02 14:08:22 -0500

    added template files and navigation route

--- App.js
+++ App.js
@@ -1,20 +1,132 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import React from "react";
+
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet,View , Text, Button } from "react-native";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+const TopTab = createMaterialTopTabNavigator();
+
+const TopTabNavigator = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TopTab.Navigator>
+      <TopTab.Screen name="Home" component={MainStackNavigator} />
+      <TopTab.Screen name="Contact" component={ContactStackNavigator} />
+    </TopTab.Navigator>
   );
 }
 
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={MainStackNavigator} />
+      <Tab.Screen name="Contact" component={ContactStackNavigator} />
+    </Tab.Navigator>
+  );
+}
+
+const screenOptionStyle = {
+  headerStyle: {
+    backgroundColor: "#9AC4F8",
+  },
+  headerTintColor: "white",
+  headerBackTitle: "Back",
+};
+
+const MainStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="About" component={About} />
+    </Stack.Navigator>
+  );
+};
+
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.center}>
+      <Text>This is the home screen</Text>
+      <Button
+        title="Go to About Screen"
+        onPress={() => navigation.navigate("About")}
+      />
+    </View>
+  );
+};
+
+const About = () => {
+  return (
+    <View style={styles.center}>
+      <Text>This is the about screen</Text>
+    </View>
+  );
+};
+
+const Contact = () => {
+  return (
+    <View style={styles.center}>
+      <Text>This is the about screen</Text>
+    </View>
+  );
+};
+
+
+const ContactStackNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={screenOptionStyle}>
+      <Stack.Screen name="Contact" component={Contact} />
+    </Stack.Navigator>
+  );
+};
+const DrawerNavigator = () => {
+  return (
+    <NavigationContainer>
+    <Drawer.Navigator>
+    <Drawer.Screen name="TopTab" component={TopTabNavigator} />
+
+      <Drawer.Screen name="Home" component={BottomTabNavigator} />
+      <Drawer.Screen name="Contact" component={ContactStackNavigator} />
+    </Drawer.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
 const styles = StyleSheet.create({
-  container: {
+  center: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
   },
 });
+
+
+export default DrawerNavigator;
