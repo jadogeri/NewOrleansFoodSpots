1739c6d94ae4dad4e0b37af4119bd5539c0635c1
jadogeri <jadogeri@uno.edu>
2025-01-27 07:47:30 -0600

    modified user.test.ts

--- client/tests/unit/models/user.test.ts
+++ client/tests/unit/models/user.test.ts
@@ -1,17 +1,25 @@
 import { User } from '../../../src/models/User';
 
 describe('testing user model and functionality', () => {
-    test('username setters and getters', async () => {
+  test('username setters and getters', async () => {
 
-      const user = new User();
-      user.setUsername("John Doe"); 
+    const user = new User();
+    user.setUsername("John Doe"); 
 
-      expect(user.getUsername()).toEqual("John Doe")
+    expect(user.getUsername()).toEqual("John Doe")
 
-  })
-    
+  });    
 
-})
+  test('email setters and getters', async () => {
+
+    const user = new User();
+    user.setEmail("JohnDoe@gmail.com"); 
+
+    expect(user.getEmail()).toEqual("JohnDoe@gmail.com")
+
+  }); 
+
+});
 
 
 
