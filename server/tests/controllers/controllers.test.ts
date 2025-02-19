e96ba59ab0901f9249f8ad39970ab809c05a7b26
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-02-18 23:53:20 -0600

    updated controllers.test.ts

--- server/tests/controllers/controllers.test.ts
+++ server/tests/controllers/controllers.test.ts
@@ -8,6 +8,11 @@ import { forgotUserTest } from "./forgotUserTest";
 import { resetUserTest } from "./resetUserTest";
 import {deactivateUserTest } from "./deactivateUserTest";
 
+import {  test } from '@jest/globals';
+const request = require('supertest');
+import { fileReader } from "../fileReader";
+const {BASE_URL}  = require("../constants")
+
 describe('testing user and contact api requests', () => {
 
     
@@ -29,7 +34,32 @@ describe('testing user and contact api requests', () => {
   });
 
     global.localStorage = new LocalStorage('./tests/storage');
-    registerUserTest();
+
+    test('registers user Tesing in isolation', async () => {
+
+      console.log(__dirname)
+      let path  : string = __dirname + "/../__mocks__/user.json"
+      let initUser = fileReader(path)
+      console.log("init user ========================",initUser)
+      localStorage.setItem("user",initUser);      
+  
+      let mock = localStorage.getItem("user");
+      let mockObj = JSON.parse(mock as string)
+      const res = await request(BASE_URL).post('/api/users/register').send(mockObj)    
+  
+     console.log("data retrieved from test == ",JSON.stringify(res.body))
+     if(res.statusCode ===201){
+      let updatedCreds = {...mockObj,... res.body,password : mockObj.password}   
+      localStorage.setItem("user",JSON.stringify(updatedCreds, null, 2))
+  
+     }
+  
+      expect(res.statusCode).toEqual(201);
+   
+    })
+  
+    // registerUserTest();
+
     loginUserTest();
     currentUserTest()
     forgotUserTest()
@@ -41,41 +71,7 @@ describe('testing user and contact api requests', () => {
 });
 
 
-/**
- * 
- * 
- 
-
-
-import * as userService from '../../../src/services/userService';
-import * as db from "../../MongoMemoryServer"
-import User from '../../../src/models/userModel'; // Your User model
-import { IUser } from '../../../src/interfaces/IUser';
-import { validateStringEquality, validateNotEmpty } from '../../validators';
-import { log } from 'console';
-import mongoose from 'mongoose';
-const users = require("../../__mocks__/users")
-
-
-describe('userService', () => {
-
-  beforeAll(async () => {
-    
-    await db.connect()
-  });
-
-  // beforeEach(async () => {
-    
-  //   await db.connect()
-  // });
-  afterEach(async () => {
-    await db.clearDatabase()
-  });
-  afterAll(async () => {
-    await db.closeDatabase()
-  });
 
 
 
 
- */
