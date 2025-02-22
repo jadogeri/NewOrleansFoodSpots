d0867a75a3d605a4c9c39f8a94fa8c0f568b905d
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-02-22 06:00:15 -0600

    Updated index.ts

--- server/src/configs/cors.ts
+++ server/src/configs/cors.ts
@@ -1,9 +1,7 @@
 import { CorsOptions } from "cors";
 
-console.log(process.env.DEV_ORIGIN, process.env.PROD_ORIGIN)
 export const corsOptions : CorsOptions= {
     origin:'*', 
-    //origin:[process.env.DEV_ORIGIN, process.env.PROD_ORIGIN], 
    // allowedHeaders: ['Content-Type', 'Authorization',"Origin", "X-Requested-With" , "Accept"],    
     credentials:true,            
     optionsSuccessStatus:200,
