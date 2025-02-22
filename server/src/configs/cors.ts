eef3de818f798f6feea94b4adda5f4ed8e6b8355
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-02-22 02:15:20 -0600

    Added more allowed Headers in cors option

--- server/src/configs/cors.ts
+++ server/src/configs/cors.ts
@@ -2,7 +2,7 @@ import { CorsOptions } from "cors";
 
 export const corsOptions : CorsOptions= {
     origin:'*', 
-    allowedHeaders: ['Content-Type', 'Authorization'],    
+    allowedHeaders: ['Content-Type', 'Authorization',"Origin", "X-Requested-With" , "Accept"],    
     credentials:true,            
     optionsSuccessStatus:200,
     methods: ["GET", "POST", "PUT", "DELETE"],
