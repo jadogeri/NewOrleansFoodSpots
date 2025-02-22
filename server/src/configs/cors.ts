2de1375a5aa31f20130c41f3296bc22c473624cb
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-02-22 02:09:11 -0600

    updated cors - added allowedHeaders

--- server/src/configs/cors.ts
+++ server/src/configs/cors.ts
@@ -2,6 +2,7 @@ import { CorsOptions } from "cors";
 
 export const corsOptions : CorsOptions= {
     origin:'*', 
+    allowedHeaders: ['Content-Type', 'Authorization'],    
     credentials:true,            
     optionsSuccessStatus:200,
     methods: ["GET", "POST", "PUT", "DELETE"],
